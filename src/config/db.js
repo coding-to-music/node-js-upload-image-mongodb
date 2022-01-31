b7059099db1e9169073c6f803f72d63e7c577f8f
coding-to-music <connors.tom@gmail.com>
2022-01-31 03:12:32 +0000

    build

--- src/config/db.js
+++ src/config/db.js
@@ -1,3 +1,9 @@
+// module.exports = {
+//   url: process.env.MONGO_URL,
+//   database: "image_files_db",
+//   imgBucket: "photos",
+// };
+
 module.exports = {
   url: "mongodb://localhost:27017/",
   database: "bezkoder_files_db",
--- src/server.js
+++ src/server.js
@@ -2,16 +2,20 @@ const cors = require("cors");
 const express = require("express");
 const app = express();
 const initRoutes = require("./routes");
+require("dotenv/config");
+
+console.log(process.env.MONGO_URL);
+console.log(process.env.PORT);
 
 var corsOptions = {
-  origin: "http://localhost:8081"
+  origin: "http://localhost:8081",
 };
 
 app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: true }));
 initRoutes(app);
 
-let port = 8080;
+let port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Running at localhost:${port}`);
 });
